8643d5f3218b3ba5bda396f8b7966402d8e75944
Zaza28 <zainebharzi26@gmail.com>
2025-11-21 19:54:22 +0100

    add all

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,23 @@
 import React from "react";
 
 const Card = ({ movie }) => {
+  new Date().toLocaleDateString("fr-FR");
   return (
     <div>
       <div id="movie-card">
         <img
-          // src={`/gIAYMDb5mIAeCAj76q1sRsKjkzo.jpg + ${movie.poster_path}`}
-          src={movie.id}
+          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
           alt="affiche du film"
         />
         <h3>{movie.original_title}</h3>
-        <p>{movie.release_date}</p>
+        <p>{new Date(movie.release_date).toLocaleDateString("fr-FR")}</p>
+        <p>{Math.round(movie.vote_average * 10) / 10}/10</p>
+        <p>
+          {movie.genre_names
+            ? movie.genre_names.join(", ")
+            : "Genres non disponibles"}
+        </p>
+        <p>{movie.overview}</p>
       </div>
     </div>
   );
--- src/pages/Data.js
+++ src/pages/Data.js
@@ -3,22 +3,60 @@ import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
 const Data = () => {
   const [data, setData] = useState("");
-
+  const [genres, setGenres] = useState("");
   useEffect(() => {
     axios
       .get(
-        "https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=code&language=fr-FR"
+        "https://api.themoviedb.org/3/search/movie?api_key=3ab561c0c6316fbd756ca83ef83aba38&query=code&language=fr-FR"
       )
       .then((res) => {
         setData(res.data.results);
         console.log(res);
       });
   }, []);
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/genre/movie/list?api_key=3ab561c0c6316fbd756ca83ef83aba38&language=en-US"
+      )
+      .then((res) => {
+        setGenres(res.data.genres);
+        console.log(res);
+      });
+  }, []);
+
+  const getMoviesWithGenres = () => {
+    if (!data || !genres) return [];
+
+    return data.map((movie) => {
+      // Trouver les noms des genres pour ce film
+      const genreNames = movie.genre_ids
+        .map((genreId) => {
+          const genre = genres.find((g) => g.id === genreId);
+          return genre ? genre.name : "";
+        })
+        .filter((name) => name !== "");
+
+      // Retourner le film avec les noms de genres ajoutés
+      return {
+        ...movie,
+        genre_names: genreNames,
+      };
+    });
+  };
 
   return (
     <div>
       <div className="movies-container">
-        {data && data.map((movie) => <Card key={movie.id} movie={movie} />)}
+        {data &&
+          genres &&
+          getMoviesWithGenres().map((movie) => (
+            <Card key={movie.id} movie={movie} />
+          ))}
+      </div>
+      <div>
+        {genres &&
+          genres.map((genre) => <span key={genre.id}>{genre.name}</span>)}
       </div>
     </div>
   );
