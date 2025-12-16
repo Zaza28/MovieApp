c52b42a0ea9ef851b1a675f13fcc807f974f08f6
Zaza28 <zainebharzi26@gmail.com>
2025-12-16 23:01:04 +0100

    add changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,26 @@
 import React from "react";
-
+import "../css/Card.css";
 const Card = ({ movie, genre, favori, isOnFavoritePage }) => {
   new Date().toLocaleDateString("fr-FR");
 
   return (
-    <div>
-      <div id="movie-card">
-        <h3>{movie.title}</h3>
-
-        <img
-          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-          alt="affiche du film"
-        />
+    <div id="card-container">
+      <h3>{movie.title}</h3>
+      <img
+        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+        alt="affiche du film"
+      />
+      <div className="movie-card-content">
         <p>{new Date(movie.release_date).toLocaleDateString("fr-FR")}</p>
         <p>{Math.round(movie.vote_average * 10) / 10}/10</p>
         <p>{genre ? genre.join(", ") : "genres non disponibles"}</p>
         <p>{movie.overview ? movie.overview : "Synopsis non disponible"}</p>
-        <button onClick={() => favori(movie)}>
-          {isOnFavoritePage
-            ? "Supprimer des coups de cœur"
-            : "Ajouter aux coups de cœur"}
-        </button>
       </div>
+      <button onClick={() => favori(movie)}>
+        {isOnFavoritePage
+          ? "Supprimer des coups de cœur"
+          : "Ajouter aux coups de cœur"}
+      </button>
     </div>
   );
 };
--- src/components/Data.js
+++ src/components/Data.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import "../css/Card.css";
 import Card from "./Card";
 
 const Data = ({ favorites, setFavorites, searchTerm, sortOrder }) => {
